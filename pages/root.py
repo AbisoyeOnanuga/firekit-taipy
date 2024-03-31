79d3c511549a751dd950fa8b53235fe75b1437cd
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-03-31 12:58:51 -0400

    update gitignore root data

--- pages/root.py
+++ pages/root.py
@@ -2,7 +2,7 @@ from taipy.gui import Markdown
 
 import numpy as np
 
-from data.data import data
+from pages.data.data import data
 
 selector_location = list(np.sort(data['Location'].astype(str).unique()))
 selected_location  = 'North Arm Stuart Lake'
