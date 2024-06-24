c2f0baee9ed17ba8584565f8feb807c1fb8e248c
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-06-24 19:49:35 -0400

    update table py

--- pages/table/table.py
+++ pages/table/table.py
@@ -1,9 +1,10 @@
 # import the necessary modules and libraries
 from taipy.gui import Markdown
-
-# import dataframe function from data/data.py
+from ..data.data import fetch_data
 
 # define data functions for the table page
+# Fetch the data
+data = fetch_data()
 
 # create a Markdown control and set the content to the Taipy table syntax
 table_md = Markdown("pages/table/table.md")
