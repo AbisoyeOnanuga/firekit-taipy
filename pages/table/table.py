559f5f0ae5dd688afa4a851cd234c8df0b203689
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-06-26 14:54:57 -0400

    update table py

--- pages/table/table.py
+++ pages/table/table.py
@@ -1,16 +1,9 @@
-import taipy.gui.builder as tgb
-from pages.data.data import fetch_data
+from taipy.gui import Markdown
+
+from ..data.data import fetch_data
 
 # Fetch the data
 data = fetch_data()
 
-# Define a function to create the table page using Taipy GUI Builder
-def create_table_page():
-    with tgb.Page() as page:
-        # Create a table element
-        tgb.table(data, page_size=10, page_size_options=[10, 50, 100], filter=True, group_by='Fire Centre', apply='Estimated Size (Ha)')
-
-    return page
-
-# Create the table page object
-table_page = create_table_page()
+# Create a Markdown control and set the content to the Taipy table syntax
+table_md = Markdown("pages/table/table.md")
